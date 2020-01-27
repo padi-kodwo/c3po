6b1fe1e4402d7c46b921890395302b42fa8aa249
Your Name <you@example.com>
2020-01-27 10:14:33 +0000

    Downloaded some JQuery libraries updated the Ajax endpoint in javascript

--- src/util/util.py
+++ src/util/util.py
@@ -12,10 +12,11 @@ def save_audio_request(audio_wav):
     if audio_wav is None:
         logger.info("")
         return None
-    elif not isinstance():
-        logger.info("")
-        return None
+    # elif not isinstance():
+    #     logger.info("")
+    #     return None
     else:
         files_destination = os.path.join(system_paths.data_store, audio_wav)
         audio_wav.save(files_destination)
+        audio_wav.read(files_destination)
         return files_destination
